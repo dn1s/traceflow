915e6f5b32d3ec4ebe04166ccf82d4b65cd53edb
rucarrol <ruairi.carroll@gmail.com>
2019-10-20 20:28:10 +0100

    - adding unit tests for __main__ - exposing functions from __main__.py for unittest

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -4,18 +4,17 @@ import traceflow
 
 class Test__main__(unittest.TestCase):
     def test_ipid_to_ints(self):
-        (i,j) = traceflow.__main__.ipid_to_ints(257)
+        (i, j) = traceflow.__main__.ipid_to_ints(257)
         self.assertIsInstance(i, int)
         self.assertIsInstance(j, int)
         self.assertEqual(i, 1)
         self.assertEqual(j, 1)
 
-
     def test_ints_to_ipid(self):
-        ipid = traceflow.__main__.ints_to_ipid(1,1)
+        ipid = traceflow.__main__.ints_to_ipid(1, 1)
         self.assertIsInstance(ipid, int)
         self.assertEqual(ipid, 257)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
